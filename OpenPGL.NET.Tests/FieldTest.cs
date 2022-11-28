eaded4162c390acdf268c2eb8a0f17645e5ec6cd
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2022-11-28 18:05:36 +0100

    refactor

--- OpenPGL.NET.Tests/FieldTest.cs
+++ OpenPGL.NET.Tests/FieldTest.cs
@@ -1,68 +1,74 @@
 using System.Numerics;
 using Xunit;
 
-namespace OpenPGL.NET.Tests {
-    public class FieldTest {
-        [Fact]
-        public void RegionShouldBeFound() {
-            using Field field = new();
+namespace OpenPGL.NET.Tests;
 
-            // Generate a batch of dummy samples on the xy plane in [0,1]x[0,1]
-            uint num = 100000;
-            OpenPGL.NET.SampleStorage samples = new();
-            samples.Reserve(num, 0);
-            System.Random rng = new(1337);
-            for (int i = 0; i < num; ++i)
-                samples.AddSample(new() {
-                    Position = new((float)rng.NextDouble(), (float)rng.NextDouble(), 0),
-                    Direction = Vector3.UnitZ,
-                    Distance = 10,
-                    Pdf = 1,
-                    Weight = 42
-                });
+public class FieldTest
+{
+    [Fact]
+    public void RegionShouldBeFound()
+    {
+        using Field field = new();
 
-            // Train the model
-            field.Update(samples, 1);
-
-            // Check that a valid region is found in the center
-            SurfaceSamplingDistribution distribution = new(field);
-            var u = 0.5f;
-            distribution.Init(new(0.5f, 0.5f, 0), ref u);
-            var region = distribution.Region;
-
-            Assert.True(region.IsValid);
-        }
-
-        [Fact]
-        public void RegionShouldBeFound_NoKnn() {
-            using Field field = new(new() {
-                SpatialSettings = new KdTreeSettings() { KnnLookup = false }
+        // Generate a batch of dummy samples on the xy plane in [0,1]x[0,1]
+        uint num = 100000;
+        OpenPGL.NET.SampleStorage samples = new();
+        samples.Reserve(num, 0);
+        System.Random rng = new(1337);
+        for (int i = 0; i < num; ++i)
+            samples.AddSample(new()
+            {
+                Position = new((float)rng.NextDouble(), (float)rng.NextDouble(), 0),
+                Direction = Vector3.UnitZ,
+                Distance = 10,
+                Pdf = 1,
+                Weight = 42
             });
 
-            // Generate a batch of dummy samples on the xy plane in [0,1]x[0,1]
-            uint num = 100000;
-            OpenPGL.NET.SampleStorage samples = new();
-            samples.Reserve(num, 0);
-            System.Random rng = new(1337);
-            for (int i = 0; i < num; ++i)
-                samples.AddSample(new() {
-                    Position = new((float)rng.NextDouble(), (float)rng.NextDouble(), 0),
-                    Direction = Vector3.UnitZ,
-                    Distance = 10,
-                    Pdf = 1,
-                    Weight = 42
-                });
+        // Train the model
+        field.Update(samples, 1);
 
-            // Train the model
-            field.Update(samples, 1);
+        // Check that a valid region is found in the center
+        SurfaceSamplingDistribution distribution = new(field);
+        var u = 0.5f;
+        distribution.Init(new(0.5f, 0.5f, 0), ref u);
+        var region = distribution.Region;
 
-            // Check that a valid region is found in the center
-            SurfaceSamplingDistribution distribution = new(field);
-            var u = 0.5f;
-            distribution.Init(new(0.5f, 0.5f, 0), ref u);
-            var region = distribution.Region;
+        Assert.True(region.IsValid);
+    }
 
-            Assert.True(region.IsValid);
-        }
+    [Fact]
+    public void RegionShouldBeFound_NoKnn()
+    {
+        using Field field = new(new()
+        {
+            SpatialSettings = new KdTreeSettings() { KnnLookup = false }
+        });
+
+        // Generate a batch of dummy samples on the xy plane in [0,1]x[0,1]
+        uint num = 100000;
+        OpenPGL.NET.SampleStorage samples = new();
+        samples.Reserve(num, 0);
+        System.Random rng = new(1337);
+        for (int i = 0; i < num; ++i)
+            samples.AddSample(new()
+            {
+                Position = new((float)rng.NextDouble(), (float)rng.NextDouble(), 0),
+                Direction = Vector3.UnitZ,
+                Distance = 10,
+                Pdf = 1,
+                Weight = 42
+            });
+
+        // Train the model
+        field.Update(samples, 1);
+
+        // Check that a valid region is found in the center
+        SurfaceSamplingDistribution distribution = new(field);
+        var u = 0.5f;
+        distribution.Init(new(0.5f, 0.5f, 0), ref u);
+        var region = distribution.Region;
+
+        Assert.True(region.IsValid);
     }
 }
